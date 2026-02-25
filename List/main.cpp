4036c390ee2df524da2136f10f05cbe80654c7ba
Илья Щербаков <iliya309@mail.ru>
2026-02-25 15:52:06 +1000

    List DONE

--- List/main.cpp
+++ List/main.cpp
@@ -15,24 +15,133 @@ public:
 	Element(int Data, Element* pPrev = nullptr, Element* pNext = nullptr):Data(Data),pPrev(pPrev),pNext(pNext)
 	{
 		count++;
+#ifdef DEBUG
 		cout << "EConstructor:\t" << this << endl;
+#endif // DEBUG
+
 	}
 	~Element()
 	{
 		count--;
+#ifdef DEBUG
 		cout << "EDestructor:\t" << this << endl;
+#endif // DEBUG
+
 	}
 	friend class List;
+	friend class Iterator;
+	friend class ConstIterator;
+	friend List operator+ (const List& left, const List& right);
 };
 int Element::count = 0;
 
-
+class Iterator
+{
+	Element* Temp;
+public:
+	Iterator(Element* Temp = nullptr) :Temp(Temp)
+	{
+		cout << "ItConstructor:\t" << this << endl;
+	}
+	~Iterator()
+	{
+		cout << "ItDestructor:\t" << this << endl;
+	}
+	Iterator& operator++()
+	{
+		Temp = Temp->pNext;
+		return *this;
+	}
+	Iterator operator++(int)
+	{
+		Iterator old = *this;
+		Temp = Temp->pNext;
+		return old;
+	}
+	bool operator==(const Iterator& other) const
+	{
+		return this->Temp == other.Temp;
+	}
+	bool operator!=(const Iterator& other) const
+	{
+		return this->Temp != other.Temp;
+	}
+	int& operator*()
+	{
+		return Temp->Data;
+	}
+};
+class ConstIterator
+{
+	Element* Temp;
+public:
+	ConstIterator(Element* Temp = nullptr) :Temp(Temp)
+	{
+		cout << "ItConstructor:\t" << this << endl;
+	}
+	~ConstIterator()
+	{
+		cout << "ItDestructor:\t" << this << endl;
+	}
+	ConstIterator& operator++()
+	{
+		Temp = Temp->pNext;
+		return *this;
+	}
+	ConstIterator operator++(int)
+	{
+		ConstIterator old = *this;
+		Temp = Temp->pNext;
+		return old;
+	}
+	bool operator==(const ConstIterator& other) const
+	{
+		return this->Temp == other.Temp;
+	}
+	bool operator!=(const ConstIterator& other) const
+	{
+		return this->Temp != other.Temp;
+	}
+	int operator*() const
+	{
+		return Temp->Data;
+	}
+};
 class List
 {
 	Element* Head;
 	Element* Tail;
 	int size;
 public:
+	ConstIterator begin() const
+	{
+		return Head;
+	}
+	ConstIterator end() const
+	{
+		return nullptr;
+	}
+	Iterator begin()
+	{
+		return Head;
+	}
+	Iterator end()
+	{
+		return nullptr;
+	}
+
+	int get_size() const
+	{
+		return size;
+	}
+	Element* get_head()const
+	{
+		return Head;
+	}
+	Element* get_tail()const
+	{
+		return Tail;
+	}
 	List()
 	{
 		Head = nullptr;
@@ -47,11 +156,20 @@ public:
 	}
 	List(const List& other) : List()
 	{
-
+		*this = other;
+		cout << "LCopyConstructor:\t" << this << endl;
 	}
-	List(const List&& other) :List()
+	List(List&& other) :List()
 	{
-
+		*this = std::move(other);
+		cout << "LMoveConstructor:\t" << this << endl;
+	}
+	List(const std::initializer_list<int>& il) :List()
+	{
+		for (int const* it = il.begin(); it != il.end(); it++)
+		{
+			push_back(*it);
+		}
 	}
 	~List()
 	{
@@ -157,22 +275,39 @@ public:
 	List& operator= (const List& other)
 	{
 		if (this == &other) return *this;
-			while (Head)pop_front();
+		while (Head)pop_front();
 		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)
 			push_front(Temp->Data);
 		cout << "LCopyAssignment:\t" << this << endl;
 		return *this;
 	}
 
+	int& operator[](int index)const
+	{
+		Element* Temp = Head;
+		for (int i = 0; i < index; i++) Temp = Temp->pNext;
+		return Temp->Data;
+	}
 };
 
+List operator+ (const List& left, const List& right)
+{
+	List result = left;
+	for (ConstIterator it = right.begin(); it != right.end(); ++it)
+	{
+		//*it *= 100;
+		result.push_back(*it);
+	}
+	return result;
+}
 
-#define BASE_CHECK
+
+//#define BASE_CHECK
 //#define HOMEWORK1
 //#define COPY_SEMANTIC_CHECK
 //#define MOVE_SEMANTIC_CHECK
 //#define RANGE_BASED_FOR_LIST
-//#DEFINE ITERATORS_CHECK
+#define ITERATORS_CHECK
 int main()
 {
 	setlocale(LC_ALL, "");
@@ -198,7 +333,7 @@ int main()
 
 #ifdef HOMEWORK1
 
-	ForwardList list(5);
+	List list(5);
 	list.print();
 	for (int i = 0; i < list.get_size(); i++)
 	{
@@ -213,40 +348,39 @@ int main()
 	cout << endl;
 #endif // HOMEWORK1
 #ifdef COPY_SEMANTIC_CHECK
-	ForwardList list1;
+	List list1;
 	list1.push_back(3);
 	list1.push_back(5);
 	list1.push_back(8);
 	list1.push_back(13);
 	list1.push_back(21);
-	list1 = list1;
-	list1.print();
+	//list1 = list1;
+	//list1.print();
 
-	ForwardList list2 = list1;
-	ForwardList list3 = list1 + list2;
+	List list3 = list1;
 	//list3.print();
 	//list1.print();]
 	list3.print();
 #endif // COPY_SEMANTIC_CHECK
 #ifdef MOVE_SEMANTIC_CHECK
-	ForwardList list1;
+	List list1;
 	list1.push_back(3);
 	list1.push_back(5);
 	list1.push_back(8);
 	list1.push_back(13);
 	list1.push_back(21);
 
-	ForwardList list2;
+	List list2;
 	list2.push_back(34);
 	list2.push_back(55);
 	list2.push_back(89);
 
-	ForwardList list3 = list1 + list2;
+	List list3 = list1 + list2;
 	list3.print();
 #endif // MOVE_SEMANTIC_CHECK
 
 #ifdef RANGE_BASED_FOR_LIST
-	ForwardList list = { 3,5,8,13,21 };
+	List list = { 3,5,8,13,21 };
 	//list.print();
 	for (int i : list)
 	{
@@ -261,9 +395,9 @@ int main()
 #endif // RANGE_BASED_FOR_LIST
 
 #ifdef ITERATORS_CHECK
-	ForwardList list1 = { 3,5,8,13,21 };
-	ForwardList list2 = { 34,55,89 };
-	ForwardList list3 = list1 + list2;
+	List list1 = { 3,5,8,13,21 };
+	List list2 = { 34,55,89 };
+	List list3 = list1 + list2;
 	for (int i : list1) cout << i << tab; cout << endl;
 	for (int i : list2) cout << i << tab; cout << endl;
 	for (int i : list3) cout << i << tab; cout << endl;
